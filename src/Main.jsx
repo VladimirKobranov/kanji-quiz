58d195768618edac2559c9f1b41768613c6ee67a
VladimirKobranov <Josephclturok@gmail.com>
2023-04-27 13:42:36 +0800

    push

---
+++ src/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Center} from "@chakra-ui/react";
+
+function Main() {
+    return (
+        <Center bg='tomato' h='100px' color='white'>
+            This is the Center
+        </Center>
+    )
+}
+
+export default Main
