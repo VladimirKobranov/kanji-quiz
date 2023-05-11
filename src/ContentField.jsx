de9536bc14a0ca17b5d69afa9d74884a3e8858db
VladimirKobranov <Josephclturok@gmail.com>
2023-05-11 22:41:14 +0800

    push

--- src/ContentField.jsx
+++ src/ContentField.jsx
@@ -1,74 +1,96 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {Box, SimpleGrid, Text, VStack} from "@chakra-ui/react";
-import style from './App.module.css';
-import {InputsContext, LevelContext} from './Main';
+import style from "./App.module.css";
+import {InputsContext, LevelContext} from "./Main";
 import KanjiCard from "./KanjiCard";
 
 function ContentField() {
     const {buttonStatus: levelButtonStatus} = useContext(LevelContext);
     const {buttonStatus: inputsButtonStatus} = useContext(InputsContext);
 
-    const activeLevelButtonIndexes = levelButtonStatus.reduce((acc, button, index) => {
-        if (button.active) {
-            acc.push(index + 1);
-        }
-        return acc;
-    }, []);
+    const [jsonData, setJsonData] = useState([]);
+    const [activeLevelButtonIndexes, setActiveLevelButtonIndexes] = useState([]);
+    const [activeInputsButtonNames, setActiveInputsButtonNames] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    const activeInputsButtonNames = inputsButtonStatus.reduce((acc, button) => {
-        if (button.active) {
-            acc.push(button.name);
-        }
-        return acc;
-    }, []);
+
+//    from David Gouveia
+//  https://github.com/davidluzgouveia
+    const url =
+        "https://raw.githubusercontent.com/davidluzgouveia/kanji-data/master/kanji.json";
+
+    function pullJson() {
+        setIsLoading(true);
+        fetch(url)
+            .then((response) => response.json())
+            .then((responseData) => {
+                const filteredData = Object.keys(responseData).filter((key) => {
+                    const jlptLevel = responseData[key].jlpt_new;
+                    return activeLevelButtonIndexes.includes(jlptLevel);
+                });
+                setJsonData(filteredData);
+                setIsLoading(false);
+            });
+    }
+
+    useEffect(() => {
+        pullJson();
+    }, [activeLevelButtonIndexes]);
+
+    useEffect(() => {
+        const levelButtons = levelButtonStatus.reduce((acc, button, index) => {
+            if (button.active) {
+                acc.push(index + 1);
+            }
+            return acc;
+        }, []);
+        setActiveLevelButtonIndexes(levelButtons);
+    }, [levelButtonStatus]);
+
+    useEffect(() => {
+        const inputButtons = inputsButtonStatus.reduce((acc, button) => {
+            if (button.active) {
+                acc.push(button.name);
+            }
+            return acc;
+        }, []);
+        setActiveInputsButtonNames(inputButtons);
+    }, [inputsButtonStatus]);
+
+    function createKanjiCard(name) {
+        return (
+            <KanjiCard
+                key={(Math.random() + 1).toString(36).substring(7)}
+                color="#E6E1E7"
+                textCol="black"
+                kanji={name}
+            />
+        );
+    }
 
     return (
-        <VStack spacing='0'>
-            <Box w='100%' h='160px'>
+        <VStack spacing="0">
+            <Box w="100%" h="160px">
                 <Text className={style.SelectedLevel}>
-                    {activeLevelButtonIndexes.length > 0 ? `N${activeLevelButtonIndexes.join(',')}` : 'Select N'}
+                    {activeLevelButtonIndexes.length > 0
+                        ? `N${activeLevelButtonIndexes.join(",")}`
+                        : "Select N"}
                 </Text>
             </Box>
-            <Box bg='gray.200' w='100%' h='900px'>
-                <SimpleGrid columns={15} spacingX='10px' spacingY='30px'>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                    <KanjiCard color='#E6E1E7' textCol='black'/>
-                    <KanjiCard color='#AF282F' textCol='white'/>
-                    <KanjiCard color='#014A77' textCol='white'/>
-                </SimpleGrid>
-                {activeInputsButtonNames.length > 0 ? activeInputsButtonNames.join(', ') : 'Select inputs'}
+            <Box bg="gray.200" w="100%" h="900px">
+                {activeInputsButtonNames.length > 0
+                    ? ""
+                    : "Select inputs"}
+                {isLoading ? (
+                    <Text>Loading...</Text>
+                ) : (
+                    <SimpleGrid columns={15} spacingX="10px" spacingY="30px">
+                        {jsonData.map(createKanjiCard)}
+                    </SimpleGrid>
+                )}
             </Box>
         </VStack>
-    )
+    );
 }
 
 export default ContentField;
--- src/KanjiCard.jsx
+++ src/KanjiCard.jsx
@@ -1,66 +1,48 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Box, Input, Text, VStack} from "@chakra-ui/react";
 import style from "./KanjiCard.module.css";
+import {InputsContext} from "./Main";
 
 function KanjiCard(props) {
+    const {buttonStatus} = useContext(InputsContext);
+
+    const renderInput = (index) => {
+        if (buttonStatus[index].active) {
+            return (
+                <Input
+                    variant="filled"
+                    className={style.CardText}
+                    placeholder={buttonStatus[index].name}
+                    size="sm"
+                    style={{width: "100px", height: "29px"}}
+                    borderRadius="2px"
+                    textAlign="center"
+                    bg={props.color}
+                    color={props.textCol}
+                    _placeholder={{opacity: 1.0, color: "#D9D7DC"}}
+                    focusBorderColor="#E6E1E7"
+                    _hover={{backgroundColor: props.color}}
+                    _focus={{backgroundColor: props.color}}
+                />
+            );
+        }
+        return null;
+    };
+
     return (
-        <VStack spacing='2px'>
-            <Box bg={props.color} w='100px' h='100px' rounded='2px' className={style.CardKanji}>
-                <Text color={props.textCol}>
-                    漢
-                </Text>
+        <VStack spacing="2px">
+            <Box bg={props.color} w="100px" h="100px" rounded="2px" className={style.CardKanji}>
+                <Text color={props.textCol}>{props.kanji}</Text>
             </Box>
-            <Box w='100px' h='auto' rounded='2px' className={style.CardText}>
-                <VStack spacing='2px'>
-                    <Input variant='filled'
-                           className={style.CardText}
-                           placeholder='romaji'
-                        //                    value={name}
-                           size="sm" style={{width: "100px", height: "29px"}}
-                           borderRadius='2px'
-                           textAlign='center'
-                           bg={props.color}
-                           color={props.textCol}
-                           _placeholder={{opacity: 1.0, color: '#D9D7DC'}}
-                           focusBorderColor='#E6E1E7'
-                           _hover={{backgroundColor: props.color}}
-                           _focus={{backgroundColor: props.color}}
-                        //                    onChange={event => setName(event.target.value)}
-                    />
-                    <Input variant='filled'
-                           className={style.CardText}
-                           placeholder='furigana'
-                        //                    value={name}
-                           size="sm" style={{width: "100px", height: "29px"}}
-                           borderRadius='2px'
-                           textAlign='center'
-                           bg={props.color}
-                           color={props.textCol}
-                           _placeholder={{opacity: 1.0, color: '#D9D7DC'}}
-                           focusBorderColor='#E6E1E7'
-                           _hover={{backgroundColor: props.color}}
-                           _focus={{backgroundColor: props.color}}
-                        //                    onChange={event => setName(event.target.value)}
-                    />
-                    <Input variant='filled'
-                           className={style.CardText}
-                           placeholder='meaning'
-                        //                    value={name}
-                           size="sm" style={{width: "100px", height: "29px"}}
-                           borderRadius='2px'
-                           textAlign='center'
-                           bg={props.color}
-                           color={props.textCol}
-                           _placeholder={{opacity: 1.0, color: '#D9D7DC'}}
-                           focusBorderColor='#E6E1E7'
-                           _hover={{backgroundColor: props.color}}
-                           _focus={{backgroundColor: props.color}}
-                        //                    onChange={event => setName(event.target.value)}
-                    />
+            <Box w="100px" h="auto" rounded="2px" className={style.CardText}>
+                <VStack spacing="2px">
+                    {buttonStatus.map((button, index) => (
+                        <React.Fragment key={index}>{renderInput(index)}</React.Fragment>
+                    ))}
                 </VStack>
             </Box>
         </VStack>
-    )
+    );
 }
 
 export default KanjiCard;
