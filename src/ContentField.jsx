d3f6bb8b15767ae566bbbb7b1ee06b318313c091
VladimirKobranov <josephclturok@gmail.com>
2023-07-25 21:52:37 +0700

    info text push

--- src/ContentField.jsx
+++ src/ContentField.jsx
@@ -84,6 +84,42 @@ function ContentField() {
         );
     }
 
+    function introMessage() {
+            return (
+                <Box width='500px'>
+                    <Text className={Content.infoGreetingsJap}>
+                        いらっしゃいませ!
+                    </Text>
+                    <Text className={Content.infoGreetingsEngl} mb='20px'>
+                        or greetings!
+                    </Text>
+                    <Text className={Content.infoGreetingsText} mb='20px'>
+                        Welcome to the Kanji Quiz App.
+                        <br/>
+                        Here, you can practice with N5 to N1 Kanji levels, along with their meanings and On'yomi and
+                        Kun'yomi readings.
+                    </Text>
+                    <Text className={Content.infoGreetingsText} mb='20px'>
+                        First, select the desired level or even several levels.<br/>
+                        Then, input your options. (On'yomi and Kun'yomi readings require a Japanese keyboard.)
+                    </Text>
+                    <Text className={Content.infoGreetingsText} mb='20px'>
+                        A blue card indicates a correct answer, while a red card indicates an incorrect one. <br/>
+                        After you're done, press the "Results" button to see your accuracy percentage.
+                    </Text>
+                    <Text className={Content.infoGreetingsText} mb='20px'>
+                        Press the "Reset" button to start over.
+                    </Text>
+                    <Text className={Content.infoGreetingsText} mb='20px'>
+                        Good luck!
+                    </Text>
+                    <Text className={Content.infoGreetingsGoodLuckJap}>
+                        頑張って!
+                    </Text>
+                </Box>
+            )
+    }
+
     return (
         <VStack spacing="0">
             <Box w="100%" h="160px">
@@ -98,7 +134,7 @@ function ContentField() {
             </Box>
             <Box w="100%" h="76vh" className={Content.scroll}>
                 <SimpleGrid columns={15} spacingX="10px" spacingY="30px" minChildWidth='100px'>
-                    {shuffledNames.map(createKanjiCard)}
+                    { levelsFromRedux.length === 0 ? introMessage() : shuffledNames.map(createKanjiCard)}
                 </SimpleGrid>
             </Box>
         </VStack>
