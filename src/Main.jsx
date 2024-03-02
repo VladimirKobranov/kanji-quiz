f1fc14e9b9444098abcb38eba8ce8949aedbffdc
VladimirKobranov <josephclturok@gmail.com>
2024-03-02 21:38:55 +0700

    upd

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,7 +2,27 @@ import React, { Suspense, useContext, useEffect, useState } from "react";
 import { StoreContext } from "./store";
 import data from "./data.js";
 import KanjiCard from "./KanjiCard";
-import { Box, Text, Button, Grid, GridItem, Center } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Button,
+  Grid,
+  GridItem,
+  Center,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  useDisclosure,
+} from "@chakra-ui/react";
+import Icon from "@mdi/react";
+import { mdiMenu } from "@mdi/js";
+import "./style.css";
 
 function shuffleArray(array) {
   const shuffledArray = [...array];
@@ -97,36 +117,116 @@ export default function Main() {
     });
   }
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
   return (
     <Box m="10px">
+      <Icon
+        className="menuIcon"
+        path={mdiMenu}
+        size={2}
+        onClick={onOpen}
+        ref={btnRef}
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="full"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <Icon
+            path={mdiMenu}
+            size={2}
+            onClick={onClose}
+            className="menuIcon"
+          />
+          <DrawerHeader bg="tomato">
+            <Center>
+              <VStack>
+                <Text>漢字 クイズ</Text>
+                <Text>Kanji Quiz</Text>
+              </VStack>
+            </Center>
+          </DrawerHeader>
+          <DrawerBody bg="gray.400">
+            <VStack>
+              <Box>
+                <VStack>
+                  <Text>level: {level}</Text>
+                  <Button onClick={handleSelectLevel(5)}>level 5</Button>
+                  <Button onClick={handleSelectLevel(4)}>level 4</Button>
+                  <Button onClick={handleSelectLevel(3)}>level 3</Button>
+                  <Button onClick={handleSelectLevel(2)}>level 2</Button>
+                  <Button onClick={handleSelectLevel(1)}>level 1</Button>
+                </VStack>
+              </Box>
+
+              <Box>
+                <VStack>
+                  <Text>input: {input}</Text>
+                  <Button onClick={handleSelectInput("meanings")}>
+                    meanings
+                  </Button>
+                  <Button onClick={handleSelectInput("readings_on")}>
+                    readings_on
+                  </Button>
+                  <Button onClick={handleSelectInput("readings_kun")}>
+                    readings_kun
+                  </Button>
+                </VStack>
+              </Box>
+
+              <Box>
+                <VStack>
+                  <Button onClick={handleResults}>results</Button>
+                  <Text>
+                    {results
+                      ? `correct: ${results.correct}, total: ${results.total}, percentage: ${results.percentage}%`
+                      : null}
+                  </Text>
+                </VStack>
+              </Box>
+
+              <Button onClick={handleReset}>reset</Button>
+
+              <Box>
+                <Button onClick={handleHintMode}>hint mode</Button>
+              </Box>
+            </VStack>
+          </DrawerBody>
+          <DrawerFooter justifyContent="center" bg="cyan">
+            copyright vk | 2024
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+
       <Suspense fallback={<Text>loading...</Text>}>
-        <Button onClick={handleResults}>results</Button>
-        <Text>
-          {results
-            ? `correct: ${results.correct}, total: ${results.total}, percentage: ${results.percentage}%`
-            : null}
-        </Text>
-        <Button onClick={handleReset}>reset</Button>
-
-        <Text>level: {level}</Text>
-        <Button onClick={handleSelectLevel(5)}>level 5</Button>
-        <Button onClick={handleSelectLevel(4)}>level 4</Button>
-        <Button onClick={handleSelectLevel(3)}>level 3</Button>
-        <Button onClick={handleSelectLevel(2)}>level 2</Button>
-        <Button onClick={handleSelectLevel(1)}>level 1</Button>
-
-        <Text>input: {input}</Text>
-        <Button onClick={handleSelectInput("meanings")}>meanings</Button>
-        <Button onClick={handleSelectInput("readings_on")}>readings_on</Button>
-        <Button onClick={handleSelectInput("readings_kun")}>
-          readings_kun
-        </Button>
-
-        <Button onClick={handleHintMode}>hint mode</Button>
-        {hintMode ? <Text>Hint mode</Text> : null}
-
-        {storeState.data.length === 0 ? <Text>select level</Text> : null}
+        <Box bg="tomato">
+          <VStack>
+            {storeState.data.length === 0 ? (
+              <Text>Select level</Text>
+            ) : (
+              <Text>N{storeState.jlpt} selected</Text>
+            )}
+            {storeState.data.length === 0 ? (
+              <Text>Select input</Text>
+            ) : (
+              <Text>{storeState.input} selected</Text>
+            )}
+            {hintMode ? <Text>Hint mode</Text> : null}
+          </VStack>
+        </Box>
         <Center>
+          {storeState.data.length === 0 ? (
+            <Box>
+              <Text>Wellcome</Text>
+              <Text>info message</Text>
+            </Box>
+          ) : null}
           <Grid templateColumns="repeat(3, 1fr)" gap={3}>
             {storeState.data.map((kanjiData, index) => (
               <GridItem key={index}>
